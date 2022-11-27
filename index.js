2f4f60662668d4fd79e162ef8554757a664988c6
jccodev <colinajosuedev@gmail.com>
2022-11-27 16:52:35 -0500

    update

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const cors = require("cors");
 const routerApi = require('./src/routes/index.js');
 
 const { logErrors, boomErrorHandler, errorHandler } = require('./src/middleware/error_handler');
 
+const port = process.env.PORT || 3000;
 const app = express();
-const port = 3000;
 
 app.use(express.json());
+
+const whiteList = [
+  'http://127.0.0.1:8080'
+];
+
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("No autorizado"));
+    }
+  }
+}
+
+app.use(cors(options));
 
 app.get('/', (req, res) =>{
   res.send('🚀Hello from my server express.js');
